b8535adc46f5a53dce73c4900ba951896008c1b5
vyshnavimuthumula <38848887+vyshnavimuthumula@users.noreply.github.com>
2018-06-18 14:46:12 +0530

    Update map.js

--- js/map.js
+++ js/map.js
@@ -3,19 +3,19 @@ var  infoWindow;
 var  locations = [
   {
     title: "Lotus Temple",
-      lat:28.5534967,lng:77.2566377
+      lat:28.5534967,lng:77.2566377,
   },  {
     title: 'Red Fort',
-      lat: 28.6561639, lng: 77.2388316
+      lat: 28.6561639, lng: 77.2388316,
   }, {
     title: 'Qutub Minar',
-      lat:28.5265723,lng: 77.1844791
+      lat:28.5265723,lng: 77.1844791,
   }, {
     title: 'India Gate',
-      lat: 28.6129167,lng: 77.227321
+      lat: 28.6129167,lng: 77.227321,
   }, {
     title: 'Jama Masjid',
-      lat: 28.6507677,lng: 77.2339893
+      lat: 28.6507677,lng: 77.2339893,
   }
 ];
 function myMap() {
@@ -27,8 +27,6 @@ function myMap() {
     zoomControl: true,
     mapTypeControl: true,
     scaleControl: true,
-    streetViewControl: true,
-    overviewMapControl: true,
     animation: google.maps.Animation.BOUNCE,
     position: google.maps.ControlPosition.TOP_CENTER,
     rotateControl: true,
@@ -71,17 +69,6 @@ function animateUponClick(marker) {
     marker.setAnimation(null);
   }, 2000);
 }
-//for different coloured markers
-function makeMarkerIcon(markerColor) {
-  var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-      '|40|_|%E2%80%A2',
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(21, 34));
-  return markerImage;
-}
 function populateInfoWindow(marker, infowindow) {
   // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
@@ -94,7 +81,6 @@ function populateInfoWindow(marker, infowindow) {
     infowindow.addListener('closeclick', function() {
       infowindow.marker = null;
     });
-    var streetViewService = new google.maps.StreetViewService();
     var radius = 30;
     var  wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
     $.ajax(wikiURL,{
@@ -103,42 +89,18 @@ function populateInfoWindow(marker, infowindow) {
         async: true
       }
     }).done(function(response) {
-      var  articleStr = response[1];
+      var  articleStr = response[0];
       var  URL = 'http://en.wikipedia.org/wiki/' + articleStr;
-      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
       infowindow.setContent('<div>' +
-        '<h3>' + marker.title + '</h3>' + '</div><br><a href ="' + URL + '">' + URL + '</a><hr><div id="pano"></div>');
+        '<h2>' + marker.title + '</h1>' + '<h4>click the below link to view wikipedia page of '+ marker.title+'</h4><br></div><br><div><a href='+URL+'" target=_blank>'+marker.title+'</a></div><br>');
       // Open the infowindow on the correct marker.
       infowindow.open(map, marker);
     }).fail(function(jqXHR, textStatus) {
-      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
       infowindow.setContent('<div>' +
-        '<h3>' + marker.title + '</h3>' + '</div><br><p>Sorry. We could not contact Wikipedia! </p><hr><div id="pano"></div>');
+        '<h3>' + marker.title + '</h3>' + '</div><br><p>Sorry. We could not contact Wikipedia! </p><hr><div></div>');
         infowindow.open(map, marker);
     });
-//The above jqXHR is a jqery api
   }
-    function getStreetView(data, status) {
-      if (status == google.maps.StreetViewStatus.OK) {
-        var nearStreetViewLocation = data.location.latLng;
-        var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, marker.position);
-        // infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-        var panoramaOptions = {
-          position: nearStreetViewLocation,
-          pov: {
-            heading: heading,
-            pitch: 30
-          }
-        };
-        var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
-      } else {
-        infowindow.setContent('<div>' + marker.title + '</div>' + '<div>No Street View Found</div>');
-      }
-    }
-  // Wikipedia API Ajax request - sampled from udacity lecture, need to add additional msg if there are no relevant wiki links to selected place.
-
-    
-
 } //end populateInfoWindow
 var googleError = function() {
   vm.mapError(true);
